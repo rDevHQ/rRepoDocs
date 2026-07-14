6e6c998e429f91df720e8f5bccc34351990bf5f1
rDevHQ <ragonline@pm.me>
2026-07-14 17:06:17 +0200

    Move share worker to rDevHQ account

--- composeApp/src/commonMain/kotlin/com/rdev/rrepodocs/data/share/ShareWorkerConfig.kt
+++ composeApp/src/commonMain/kotlin/com/rdev/rrepodocs/data/share/ShareWorkerConfig.kt
@@ -1,5 +1,5 @@
 package com.rdev.rrepodocs.data.share
 
 object ShareWorkerConfig {
-    const val defaultBaseUrl: String = "https://rrepodocs-share-worker.rdevhq.workers.dev"
+    const val defaultBaseUrl: String = "https://rrepodocs-share-worker.ragonline.workers.dev"
 }
